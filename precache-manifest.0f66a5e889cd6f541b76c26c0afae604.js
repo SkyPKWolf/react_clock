d3a19d3abb52d82f557ab4b070fbe0933a524d1c
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-05-20 12:08:49 +0300

    Updates

---
+++ precache-manifest.0f66a5e889cd6f541b76c26c0afae604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bb5d7cc585351425d3429ba587d500",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21a12d411acccf8b3f08",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a12d411acccf8b3f08",
+    "url": "./static/js/main.16176b86.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
